78d74254fc0a1964c7535d974afd499c8b257f7e
Danp06 <dgnecco@utb.edu.co>
2022-09-12 18:19:27 -0500

    Cambio de variables y documentacion

--- src/main/java/Alienigena.java
+++ src/main/java/Alienigena.java
@@ -16,40 +16,4 @@ public class Alienigena extends Personajes {
     public Alienigena(String name, int edad, double estatura, double peso, double energia, Sexo sexo) {
         super(name, edad, estatura, peso, energia, sexo);
     }
-
-    /**
-     * Do something.
-     *
-     * @param x the personaje
-     */
-    @Override
-    public void doSomething(Personajes x) {
-        super.doSomething(x);
-    }
-
-    /**
-     * @param a una caracteriztica
-     */
-    @Override
-    public void Add(Caracterizacion[] a) {
-        super.Add(a);
-    }
-
-    /**
-     * @param a la liga que perteneze
-     */
-    @Override
-    public void Liga(Ligas a) {
-        super.Liga(a);
-    }
-
-    /**
-     * @param x el personaje
-     * @param y el nombre del enemigo
-     * @return El enemigo de dicho personaje
-     */
-    @Override
-    public Personajes Enemigo(Personajes x, String y) {
-        return super.Enemigo(x, y);
-    }
 }
--- src/main/java/Artificial.java
+++ src/main/java/Artificial.java
@@ -3,43 +3,17 @@
  */
 public class Artificial extends Personajes {
 
+    /**
+     * Instantiates a new Artificial.
+     *
+     * @param name     the name
+     * @param edad     the edad
+     * @param estatura the estatura
+     * @param peso     the peso
+     * @param energia  the energia
+     * @param sexo     the sexo
+     */
     public Artificial(String name, int edad, double estatura, double peso, double energia, Sexo sexo) {
         super(name, edad, estatura, peso, energia, sexo);
-    }
-
-    /**
-     * Do something.
-     *
-     * @param x the personaje
-     */
-    @Override
-    public void doSomething(Personajes x) {
-        super.doSomething(x);
-    }
-
-    /**
-     * @param a una caracteriztica
-     */
-    @Override
-    public void Add(Caracterizacion[] a) {
-        super.Add(a);
-    }
-
-    /**
-     * @param a la liga que perteneze
-     */
-    @Override
-    public void Liga(Ligas a) {
-        super.Liga(a);
-    }
-
-    /**
-     * @param x el personaje
-     * @param y el nombre del enemigo
-     * @return El enemigo de dicho personaje
-     */
-    @Override
-    public Personajes Enemigo(Personajes x, String y) {
-        return super.Enemigo(x, y);
     }
 }
